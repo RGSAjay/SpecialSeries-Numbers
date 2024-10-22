49381fed0951b685e51c6caaa14823b316515ba3
RAVULAPALLI GURU SANKAR AJAY <ajayrgs123@gmail.com>
2024-10-22 19:02:27 +0530

    Bind Flask app to 0.0.0.0 and dynamic port for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -129,4 +130,6 @@ def check_automorphic(num):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to the environment port if available, default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
